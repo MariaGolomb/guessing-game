56aeba323dae69316144d016b23534a2572655e4
MariaGolomb <marialukashuk.ml@gmail.com>
2019-10-07 13:02:50 +0300

    solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,19 +1,31 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this._min=0;
+        this._max=0;
+        this._candidate=null;
+    }
 
     setRange(min, max) {
-
+        this._min=min;
+        this._max=max;
     }
 
     guess() {
+        let candidate=Math.round((this._min+this._max)/2);
+        this._candidate=candidate;
+        return candidate;
 
     }
 
     lower() {
+       this._max=Math.round((this._max+this._min)/2);
 
     }
 
     greater() {
+
+        this._min=Math.round((this._max+this._min)/2);
+    
 
     }
 }
